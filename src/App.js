32852cfba3609e1a4b5c1eb403b38ff4fb0c752f
GowthamaViknesh <gowthampostbox18@gmail.com>
2023-07-05 01:15:44 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import ContentPage from './components/Content';
+import HeaderPage from './components/Header'
+function App() {
+  return (
+    <div className="App">
+      <HeaderPage />
+      <ContentPage/>
+      </div>
+  );
+}
+
+export default App;
